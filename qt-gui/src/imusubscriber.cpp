7ac045d3e61031288ec534c2c69c1e77dffb6055
Ian Feldman <ianrfeldman@gmail.com>
2025-01-17 16:46:06 -0800

    Add imu widget to mainwindow

--- qt-gui/src/imusubscriber.cpp
+++ qt-gui/src/imusubscriber.cpp
@@ -4,13 +4,13 @@ using std::placeholders::_1;
 
 IMUSubscriber::IMUSubscriber()
     : Node("imu_subscriber") {
-      subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
+    subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
       "/imu", 10, std::bind(&IMUSubscriber::topic_callback, this, _1));
 }
 
 void IMUSubscriber::topic_callback(const sensor_msgs::msg::Imu &msg)
 {
-    RCLCPP_INFO(this->get_logger(), "subscribing to imu data");
+    // RCLCPP_INFO(this->get_logger(), "subscribing to imu data");
 
     emit dataUpdated(
         msg.orientation_covariance,
