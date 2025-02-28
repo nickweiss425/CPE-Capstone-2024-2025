32f1339847d9780338cfe3d76aa7b6a459a6afe1
Ian <ianrfeldman@gmail.com>
2025-02-28 05:44:08 +0000

    Add publisher and subscriber to each node

--- heartbeat/src/groundstation.cpp
+++ heartbeat/src/groundstation.cpp
@@ -5,7 +5,7 @@
 #include "std_msgs/msg/empty.hpp"
 
 using namespace std::chrono_literals;
-
+using std::placeholders::_1;
 
 class GroundStation : public rclcpp::Node
 {
@@ -13,20 +13,35 @@ public:
     GroundStation()
     : Node("groundstation")
     {
-        publisher_ = this->create_publisher<std_msgs::msg::Empty>("/heartbeat/send", 10);
+        /* Create publisher to send ping to the drone */
+        publisher_ = this->create_publisher<std_msgs::msg::Empty>("/heartbeat/ping", 10);
+        /* Create subscriber to receive ack from  drone */
+        subscription_ = this->create_subscription<std_msgs::msg::Empty>(
+            "/heartbeat/ack", 10, std::bind(&GroundStation::ack_callback, this, _1));
+        /* Create timer to send pings at reguler interval */
         timer_ = this->create_wall_timer(
             5000ms, std::bind(&GroundStation::timer_callback, this));
     }
 
 private:
+    /* Send ping */
     void timer_callback()
     {
-        RCLCPP_INFO(this->get_logger(), "Pinging");
+        RCLCPP_INFO(this->get_logger(), "Sending ping");
         auto msg = std_msgs::msg::Empty();
         publisher_->publish(msg);
     }
+
+    /* Recieve ack */
+    void ack_callback(const std_msgs::msg::Empty::SharedPtr msg) const
+    {
+        (void)msg;
+        RCLCPP_INFO(this->get_logger(), "Ack received");
+    }
+
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr publisher_;
+    rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr subscription_;
 };
 
 
