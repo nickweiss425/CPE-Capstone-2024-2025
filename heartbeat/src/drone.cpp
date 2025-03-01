aefde8500319b87cf81f87b8d9acbed21ffd23b6
Ian <ianrfeldman@gmail.com>
2025-03-01 19:41:23 +0000

    Start timing pings

--- heartbeat/src/drone.cpp
+++ heartbeat/src/drone.cpp
@@ -3,6 +3,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/empty.hpp"
+#include "std_msgs/msg/int32.hpp"
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -18,20 +19,41 @@ public:
             "/heartbeat/ping", 10, std::bind(&Drone::ping_callback, this, _1));
         /* Create publisher to send ack to the ground station */
         publisher_ = this->create_publisher<std_msgs::msg::Empty>("/heartbeat/ack", 10);
+        /* Create publisher to land the drone */
+        land_publisher_ = this->create_publisher<std_msgs::msg::Int32>("desired_state", 10);
+        /* Indicates if the drone has received the first ping yet */
+        initiated_ = false;
     }
 
 private:
     /* Callback function to receive ping */
-    void ping_callback(const std_msgs::msg::Empty::SharedPtr msg) const
+    void ping_callback(const std_msgs::msg::Empty::SharedPtr msg)
     {
         RCLCPP_INFO(this->get_logger(), "Ping received");
         /* Send ack */
         auto ack_msg = std_msgs::msg::Empty();
         publisher_->publish(ack_msg);
+
+        /* start measuring time between pings */
+        if (!initiated_)
+        {
+            initiated_ = true;
+            last_ping = std::chrono::steady_clock::now();
+            return;
+        }
+
+        /* measure time difference */
+        std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+        auto delta = std::chrono::duration_cast<std::chrono::microseconds>(now - last_ping).count();
+        RCLCPP_INFO(this->get_logger(), "%d", delta);
+        last_ping = now;
     }
 
+    bool initiated_;
+    std::chrono::steady_clock::time_point last_ping;
     rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr subscription_;
     rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr publisher_;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr land_publisher_;
 };
 
 
