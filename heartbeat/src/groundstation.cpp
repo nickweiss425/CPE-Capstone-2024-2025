5e297412bc8ee5d5531884e9e4a6740626050591
Ian <ianrfeldman@gmail.com>
2025-03-01 20:13:34 +0000

    Groundstation publish if it doesn't receive ack

--- heartbeat/src/groundstation.cpp
+++ heartbeat/src/groundstation.cpp
@@ -15,32 +15,49 @@ public:
     {
         /* Create publisher to send ping to the drone */
         publisher_ = this->create_publisher<std_msgs::msg::Empty>("/heartbeat/ping", 10);
+        /* Create publisher to indicate to GUI connection status  */
+        status_publisher_ = this->create_publisher<std_msgs::msg::Empty>("/heartbeat/disconnected", 10);
         /* Create subscriber to receive ack from  drone */
         subscription_ = this->create_subscription<std_msgs::msg::Empty>(
             "/heartbeat/ack", 10, std::bind(&GroundStation::ack_callback, this, _1));
         /* Create timer to send pings at reguler interval */
         timer_ = this->create_wall_timer(
             5000ms, std::bind(&GroundStation::timer_callback, this));
+        /* Monitor if an ack was received */
+        received_ack_ = true;
     }
 
 private:
     /* Send ping */
     void timer_callback()
     {
-        RCLCPP_INFO(this->get_logger(), "Sending ping");
         auto msg = std_msgs::msg::Empty();
+
+        /* Check if we received an ack since the last ping */
+        if (!received_ack_)
+        {
+            RCLCPP_INFO(this->get_logger(), "Did not recieve ACK");
+            status_publisher_->publish(msg);
+        }
+
+        RCLCPP_INFO(this->get_logger(), "Sending ping");
         publisher_->publish(msg);
+
+        received_ack_ = false;
     }
 
     /* Recieve ack */
-    void ack_callback(const std_msgs::msg::Empty::SharedPtr msg) const
+    void ack_callback(const std_msgs::msg::Empty::SharedPtr msg)
     {
         (void)msg;
         RCLCPP_INFO(this->get_logger(), "Ack received");
+        received_ack_ = true;
     }
 
+    bool received_ack_;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr publisher_;
+    rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr status_publisher_;
     rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr subscription_;
 };
 
