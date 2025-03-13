411a8fa2f4b2a39a56ddb8fde68500fa15b9350f
nickweiss425 <nicholasweiss9898@gmail.com>
2025-03-12 18:03:49 -0700

    removed old versions of gui/lora from this branch

--- lora/build/lora/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/niweiss/CPE-Capstone-2024-2025/lora
-# Build directory: /home/niweiss/CPE-Capstone-2024-2025/lora/build/lora
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
