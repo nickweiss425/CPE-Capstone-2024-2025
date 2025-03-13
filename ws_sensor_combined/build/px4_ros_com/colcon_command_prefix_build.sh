f073b8ef665aa3606b0f1c880843ed2fc28148e3
nickweiss425 <nicholasweiss9898@gmail.com>
2025-03-12 18:29:43 -0700

    updated gitignore, touch ups for drone code

--- ws_sensor_combined/build/px4_ros_com/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/niweiss/CPE-Capstone-2024-2025/ws_sensor_combined/install/gui_messages/share/gui_messages/package.sh"
-. "/home/niweiss/CPE-Capstone-2024-2025/ws_sensor_combined/install/px4_msgs/share/px4_msgs/package.sh"
